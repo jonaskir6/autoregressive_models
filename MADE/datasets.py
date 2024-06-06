23ce3a2b285af523a51ee3c3b9766d542b27e15a
jonaskir6 <116752112+jonaskir6@users.noreply.github.com>
2024-06-06 23:53:53 +0200

    fixed datasets.py & added to journal

--- MADE/datasets.py
+++ MADE/datasets.py
@@ -1,42 +1,55 @@
-import torch, torchvision
+import torch
 from torchvision import datasets, transforms    
-import matplotlib.pyplot as plt
 
 # MNIST example from paper is in binarized mnist
-class Binarize(object):
+class Binarize():
     def __call__(self, tensor):
         return (tensor > 0.5).float()
 
 class Dataset():
 
-    def __init__(self, data_dir):
+    def __init__(self, dataset, batch_size=64, data_dir='data'):
         self.data_dir = data_dir
         # Binirize MNIST
         self.transform = transforms.Compose([transforms.ToTensor(),
                               Binarize(),
                               torch.nn.Flatten(0)])
         # Train and test data
-        self.train_data = datasets.MNIST(self.data_dir, train=True, download=True, transform=self.transform)
-        self.test_data = datasets.MNIST(self.data_dir, train=False, transform=self.transform)
+        if dataset == 'mnist':
+            self.train_data = datasets.MNIST(self.data_dir, train=True, download=True, transform=self.transform)
+            self.test_data = datasets.MNIST(self.data_dir, train=False, transform=self.transform)
+        else:
+            raise ValueError('Dataset not implemented')
         # Data loaders
-        self.train_data_loader = torch.utils.data.DataLoader(self.train_data, batch_size=64, shuffle=True)
-        self.test_data_loader = torch.utils.data.DataLoader(self.test_data, batch_size=64, shuffle=False)
+        self.train_data_loader = torch.utils.data.DataLoader(self.train_data, batch_size, shuffle=True)
+        self.test_data_loader = torch.utils.data.DataLoader(self.test_data, batch_size, shuffle=False)
     
-    def get_train_data(self):
-        return self.train_data
+    def get_train_data_loader(self):
+        return self.train_data_loader
 
-    def get_test_data(self):
-        return self.test_data
+    def get_test_data_loader(self):
+        return self.test_data_loader
     
-    def visualize_dataset(self):
-        import itertools
-        images = torch.stack(tuple(zip(*tuple(itertools.islice(iter(self.test_data), 16))))[0])  # You don't need to understand this code...
-        plt.imshow(1 - torchvision.utils.make_grid(images.unflatten(1, (1, 28, 28))).permute(1, 2, 0))
-        plt.title('Your model should predict the correct number for each image.')
-        plt.axis('off')
+    def visualize_dataset(self, data_loader):
+        """
+        Visualize the dataset
+        Args:
+            data_loader: train_data_loader or test_data_loader (train or test dataset)
+        """
+        import matplotlib.pyplot as plt
+        import numpy as np
 
-    
+        # Iterator and first batch of data/lables
+        data_iter = iter(data_loader)
+        images, labels = next(data_iter)
 
-
-ds = Dataset('data')
-ds.visualize_dataset()
+        fig = plt.figure(figsize=(25, 4))
+        for idx in np.arange(20):
+            # subplot 2 rows and 10 columns, no grid, index starting from 1
+            ax = fig.add_subplot(2, 10, idx+1, xticks=[], yticks=[])
+            # Reshape the image (2D) & make it grayscale
+            ax.imshow(np.squeeze(images[idx]).view(28, 28), cmap='gray')
+            # Print the label as title
+            ax.set_title(str(labels[idx].item()))
+        plt.show()
+        
--- journal.md
+++ journal.md
@@ -1,2 +1,3 @@
+27.05: Jonas Kirch implemented datasets.py
 30.05: Niclas & Jonas Sternemann implemented networks.py together
 03.06: Ilias implemented sampling.py
