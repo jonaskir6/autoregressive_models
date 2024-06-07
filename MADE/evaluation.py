3d1bb0aca657416810fe86441464482d68e0a6c1
NiclasGri <nicigrit@gmail.com>
2024-06-07 16:37:47 +0200

    added evaluation.py

---
+++ MADE/evaluation.py
@@ -0,0 +1,25 @@
+import torch
+from torch import nn
+
+
+
+def evaluate(test_data_loader, model, device):
+    num_correct = 0
+    num_total = 0
+
+  # Disable gradient computation.
+    with torch.no_grad():
+        # Switch to evaluation mode.
+        model.eval()
+        loss=[]
+        # Iterate over the entire test dataset.
+        for images, labels in test_data_loader:
+       
+            images = images.to(device)
+            output = model(images)
+            loss.append(nn.functional.binary_cross_entropy(output,images))
+
+    loss=torch.Tensor(loss)   
+    return torch.mean(loss)
+        
+        
