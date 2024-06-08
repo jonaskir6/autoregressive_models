ad94312f08cdf4942d0f078d2ff1118fdd6483f3
IliasAttary <iliasattary1@gmail.com>
2024-06-08 02:13:14 +0200

    Refactor save_samples function to optimize grid layout for sample images

--- MADE/sampling.py
+++ MADE/sampling.py
@@ -11,7 +11,7 @@ def sample(model, num_samples=100, device='cuda'):
         samples = torch.zeros((num_samples, image_size), device=device)
         for i in range(image_size):
             logits = model(samples)  # Get the logits for the current samples
-            probs = logits[:, i]  # Convert logits to probabilities
+            probs = logits[:, i]  # Convert logits to probabilities (already done in netwoks.py as a layer)
             samples[:, i] = torch.bernoulli(probs)  # Sample from the Bernoulli distribution
 
     return samples.cpu().numpy().reshape(-1, 28, 28) # Reshape to (num_samples, 28, 28)
@@ -19,10 +19,16 @@ def sample(model, num_samples=100, device='cuda'):
 def save_samples(samples, filename='samples.png'):
     # Get the number of samples
     num_samples = samples.shape[0]
-    # We use the ceiling of the square root of the number of samples to ensure all samples fit
-    grid_size = int(np.ceil(np.sqrt(num_samples)))
 
-    fig, axs = plt.subplots(grid_size, grid_size, figsize=(grid_size * 2.8, grid_size * 2.8))
+    if(num_samples<=5):
+        grid_size_1 = 1
+        grid_size_2 = num_samples
+    else:
+        # We use the ceiling of the square root of the number of samples to ensure all samples fit
+        grid_size_1 = int(np.ceil(np.sqrt(num_samples)))
+        grid_size_2 = grid_size_1
+    
+    fig, axs = plt.subplots(grid_size_1, grid_size_2, figsize=(28, 28))
     axs = axs.flatten()
 
     for img, ax in zip(samples, axs):
