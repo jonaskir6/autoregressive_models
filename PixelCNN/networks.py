a6eba71ec43a8e056986606fd8cc8f842712438a
jonaskir6 <116752112+jonaskir6@users.noreply.github.com>
2024-06-20 00:54:36 +0200

    fixed networks.py (details in journal)

--- PixelCNN/networks.py
+++ PixelCNN/networks.py
@@ -3,6 +3,8 @@ from torch import nn
 import torch
 import numpy as np
 
+# TODO: Residual Blocks: 2h out of context or the out_channels of Mask A 256?
+
 
 class PixelCNN(nn.Module):
     """
@@ -10,21 +12,14 @@ class PixelCNN(nn.Module):
     """
     def __init__(self, num_kernels=128,  num_residual=10):
         super(PixelCNN, self).__init__()
-        
-        #TODO - Args
-        layers=[]
-        layers.append(MaskedConv2d(mask_type='A', kernel_size=7, in_channels=3 ,out_channels=num_kernels, padding=3))
-        
-        for _ in range(num_residual):
-            layers.append(ResidualBlock(num_kernels))
 
-        layers.appendMaskedConv2d(mask_type='B', kernel_size=1, padding=1)
-        layers.appendMaskedConv2d(mask_type='B', kernel_size=1, padding=1)
-        
-        layers.append(nn.ReLU())
-        layers.append(nn.functional.softmax(dim=4))
-
-        self.net=nn.ModuleList(layers)
+        self.net = nn.Sequential(
+            MaskedConv2d(mask_type='A', kernel_size=7, in_channels=3 ,out_channels=2*num_kernels, padding=3),
+            *[ResidualBlock(2*num_kernels) for _ in range(num_residual)],
+            MaskedConv2d(in_channels=2*num_kernels, out_channels=num_kernels, mask_type='B', kernel_size=1, padding=1),
+            MaskedConv2d(in_channels=num_kernels, out_channels=3, mask_type='B', kernel_size=1, padding=1),
+            nn.ReLU()
+        )
 
     def forward(self, x):
         return self.net(x)
@@ -39,30 +34,29 @@ class MaskedConv2d(nn.Conv2d):
         super(MaskedConv2d, self).__init__(in_channels, out_channels, kernel_size, padding=padding, stride=stride)
 
         # Initialize mask with 1s, don't need in/out channels here
-        self.register_buffer('mask', self.weight.data.clone())
-        _, _, kHeight, kWidth = self.weight.size()
-        self.mask.fill_(1)
+        mask = np.ones(self.weight.size(), dtype=np.float32)
+        kHeight, kWidth = kernel_size, kernel_size
 
-        # Mask out future pixels: if mask type is B, the pixel at the center of the kernel is also not masked out
-        # ??
-        self.mask[:, :, kHeight // 2, kWidth // 2 + (mask_type == 'B'):] = 0
-        self.mask[:, :, kHeight // 2 + 1:] = 0
+        # Only mask out future pixels
+        mask[:, :, :kHeight//2, :] = 1
+        mask[:, :, kHeight//2, :kWidth//2 + 1] = 1
 
+        # Create Boolean mask for each kernel to determine wether to mask out the color channel or not
         def color_mask(color_out, color_in):
             a = (np.arange(out_channels) % data_channels == color_out)[:, None]
             b = (np.arange(in_channels) % data_channels == color_in)[None, :]
             return a * b
-        
-        for i in range(data_channels):
-            for j in range(i + 1, data_channels):
-                self.mask[color_mask(i, j), kHeight // 2, kWidth // 2] = 0
-        
-        # ??
-        # if mask_type == 'A':
-        #     for k in range(data_channels):
-        #         self.mask[color_mask(k, k), kHeight // 2, kWidth // 2] = 1  
 
-        self.mask = torch.from_numpy(self.mask).float()
+        # Mask out color channels according to the Paper: R -> R, G -> RG, B -> RGB
+        for output_channel in range(data_channels):
+            for input_channel in range(output_channel + 1, data_channels):
+                mask[color_mask(output_channel, input_channel), kHeight//2, kWidth//2] = 0
+        
+        if mask_type == 'A':
+            for channel in range(data_channels):
+                mask[color_mask(channel, channel), kHeight//2, kWidth//2] = 0
+
+        self.register_buffer('mask', torch.from_numpy(mask))
 
     def forward(self, x):
         # forward pass with all values that are not masked out
@@ -75,16 +69,16 @@ class ResidualBlock(nn.Module):
     Residual Blocks:
     ReLU -> Conv1x1 -> ReLU -> MaskedConv2d -> ReLU -> Conv1x1
     """
-    def __init__(self, features):
+    def __init__(self, in_channels):
         super(ResidualBlock, self).__init__()
         
         self.net = nn.Sequential(
             nn.ReLU(),
-            MaskedConv2d(mask_type='B', in_channels=features, out_channels=features//2, kernel_size=1, padding=1),
+            MaskedConv2d(mask_type='B', in_channels=in_channels, out_channels=in_channels//2, kernel_size=1, padding=0),
             nn.ReLU(),
-            MaskedConv2d(mask_type='B', in_channels=features//2, out_channels=features//2, kernel_size=3, padding=1),
+            MaskedConv2d(mask_type='B', in_channels=in_channels//2, out_channels=in_channels//2, kernel_size=3, padding=1),
             nn.ReLU(),
-            MaskedConv2d(mask_type='B', in_channels=features//2, out_channels=features, kernel_size=1, padding=1)
+            MaskedConv2d(mask_type='B', in_channels=in_channels//2, out_channels=in_channels, kernel_size=1, padding=0)
         )
 
 
--- journal.md
+++ journal.md
@@ -7,7 +7,8 @@ MADE:
 10.06: tested various parameter settings (together)
 
 PixelCNN:
-13.06: Jonas Kirch implemented initial form of datasets.py & networks.py (classes PixelCNN, MaskedConv2d & ResidualBlock without Color Channel dependencies)
-17.06: Group Session: started completion.py, train loop and added color channel dependencies to networks.py
+13.06: Jonas implemented initial form of datasets.py & networks.py (classes PixelCNN, MaskedConv2d & ResidualBlock without Color Channel dependencies)
+17.06: Group Session: started completion.py, train loop and tried adding color channel dependencies to networks.py
 17.06: Niclas fixed Residual Blocks, added nets forward function, cleanup of variables, added mnist support for datasets.py
 19.06: Ilias finished completion.py(get_image, mask_image, complete, plot), added finished sampling.py (still needs testing when the network is fixed)
+20:06: Jonas finished fixing networks.py (color channel dependencies, mask generation, nn Sequential for all classes, Residual Blocks and in_channels & out_channels) -> PixelCNN() call works
