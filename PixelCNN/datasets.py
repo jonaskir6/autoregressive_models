fixed PixelCNN (Pixels were too dark) and added batch normalization

PixelCNN/datasets.py
@@ -14,7 +14,11 @@ class Dataset():
                 transforms.ToTensor(),
             ])
         elif dataset=='mnist':
-            self.transform = transforms.ToTensor()
+            self.transform = transforms.Compose([
+                transforms.Grayscale(num_output_channels=3),
+                transforms.Resize((32, 32)),
+                transforms.ToTensor(),
+            ])
         # Train and test data
         if dataset == 'mnist':
             self.train_data = datasets.MNIST(data_dir, train=True, download=True, transform=self.transform)
@@ -25,8 +29,8 @@ class Dataset():
         else:
             raise ValueError('Dataset not implemented')
         # Data loaders
-        self.train_data_loader = torch.utils.data.DataLoader(self.train_data, batch_size, shuffle=True)
-        self.test_data_loader = torch.utils.data.DataLoader(self.test_data, batch_size, shuffle=False)
+        self.train_data_loader = torch.utils.data.DataLoader(self.train_data, batch_size, shuffle=True, drop_last=True)
+        self.test_data_loader = torch.utils.data.DataLoader(self.test_data, batch_size, shuffle=False, drop_last=True)
     
     def get_train_data_loader(self):
         return self.train_data_loader

PixelCNN/networks.py
@@ -10,18 +10,21 @@ class PixelCNN(nn.Module):
     """
     PixelCNN Network
     """
-    def __init__(self, num_kernels=128,  num_residual=10):
+    def __init__(self, num_kernels=128,  num_residual=7, in_channels=3):
         super(PixelCNN, self).__init__()
 
         self.net = nn.Sequential(
-            MaskedConv2d(mask_type='A', kernel_size=7, in_channels=3 ,out_channels=2*num_kernels, padding=3),
+            MaskedConv2d(mask_type='A', kernel_size=7, in_channels=in_channels ,out_channels=2*num_kernels, padding=3),
             *[ResidualBlock(2*num_kernels) for _ in range(num_residual)],
+            nn.BatchNorm2d(2*num_kernels),
             nn.ReLU(),
             MaskedConv2d(in_channels=2*num_kernels, out_channels=2*num_kernels, mask_type='B', kernel_size=1, padding=0),
+            nn.BatchNorm2d(2*num_kernels),
             nn.ReLU(),
             MaskedConv2d(in_channels=2*num_kernels, out_channels=2*num_kernels, mask_type='B', kernel_size=1, padding=0),
+            nn.BatchNorm2d(2*num_kernels),
             nn.ReLU(),
-            nn.Conv2d(in_channels=2*num_kernels, out_channels=3*256, kernel_size=1, stride=1, padding=0 )
+            nn.Conv2d(in_channels=2*num_kernels, out_channels=in_channels*256, kernel_size=1, stride=1, padding=0 )
         )
 
     def forward(self, x):
@@ -77,8 +80,10 @@ class ResidualBlock(nn.Module):
         self.net = nn.Sequential(
             nn.ReLU(),
             MaskedConv2d(mask_type='B', in_channels=in_channels, out_channels=in_channels//2, kernel_size=1, padding=0),
+            nn.BatchNorm2d(in_channels//2),
             nn.ReLU(),
             MaskedConv2d(mask_type='B', in_channels=in_channels//2, out_channels=in_channels//2, kernel_size=3, padding=1),
+            nn.BatchNorm2d(in_channels//2),
             nn.ReLU(),
             MaskedConv2d(mask_type='B', in_channels=in_channels//2, out_channels=in_channels, kernel_size=1, padding=0)
         )

PixelCNN/sampling.py
@@ -39,11 +39,11 @@ def save_samples(samples, filename='samples.png'):
         grid_size_1 = int(np.ceil(np.sqrt(num_samples)))
         grid_size_2 = grid_size_1
     
-    fig, axs = plt.subplots(grid_size_1, grid_size_2, figsize=(32, 32))
+    fig, axs = plt.subplots(grid_size_1, grid_size_2, figsize=(8, 8))
     axs = axs.flatten()
 
     for img, ax in zip(samples, axs):
-        ax.imshow((img / 3 * 255).astype(int))
+        ax.imshow((img * 255).astype(int))
         ax.axis('off')
 
     # Turn off remaining empty subplots